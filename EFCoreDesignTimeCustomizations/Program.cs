6a8720d83f1152d1cdd758fe2856f10ee28b237c
Andrii Kurdiumov <kant2002@gmail.com>
2024-04-23 15:38:35 +0500

    Add project files.

---
+++ EFCoreDesignTimeCustomizations/Program.cs
@@ -0,0 +1,8 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using EFCoreDesignTimeCustomizations.Design;
+using Microsoft.EntityFrameworkCore.Scaffolding;
+
+var builder = WebApplication.CreateBuilder(args);
+//builder.Services.AddSingleton<ModelCodeGeneratorDependencies, ModelCodeGeneratorDependencies>();
+builder.Services.AddSingleton<IModelCodeGenerator, MyTextTemplatingModelGenerator>();
